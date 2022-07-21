6699b5156a3bc255e124e572c1d86424c54952b9
Bruno Saibert <brunosaibert@gmail.com>
2022-07-20 21:04:19 -0300

    fix: padrao de nomenclatura

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,19 +8,19 @@ import { AppRoutes } from "./app.routes";
 import { Loading } from "../components/Loading";
 
 export function Routes() {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState<FirebaseAuthTypes.User>();
 
   useEffect(() => {
     const subscribe = auth().onAuthStateChanged((response) => {
       setUser(response);
-      setLoading(false);
+      setIsLoading(false);
     });
 
     return subscribe;
   }, []);
 
-  if (loading) {
+  if (isLoading) {
     return <Loading />;
   }
 
